b52db698e495d051d613a4555a4bf417f2eff113
xioamage001 <mahejun126@126.com>
2026-09-12 10:45:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   filter: {
     sender: process.env.FILTER_SENDER || 'amazon',
     subject: process.env.FILTER_SUBJECT || 'Search term',
-    days: parseInt(process.env.FILTER_DAYS || '7'),
+    days: parseInt(process.env.FILTER_DAYS || '3'),
   },
 
   // 数据存储表名
